a14d7f95b5df69f4b8169e552dffd16537404960
Goodnameisfordoggy <h.dj.not.found@gmail.com>
2024-03-10 23:43:56 +0800

    FEAT: 歌单设置中的文件操作: 移动,复制,删除

--- SettingPages.py
+++ SettingPages.py
@@ -17,7 +17,7 @@ class PageSongList(QScrollArea):
             self.setStyleSheet("QScrollArea { border: transparent; }")
             self.setWidgetResizable(True) # 组件可调整大小属性
             self.fst_items_name = [sublist[0] for sublist in config_js['music_folders_path']]
-            self.selected_subitem = None
+            self.selected_subitem_AP = None
             self.construct()
 
 
@@ -76,9 +76,12 @@ class PageSongList(QScrollArea):
             children=[label1, comboBox1, label2, self.comboBox2])
         
         # widget2布局
-        button1 = PushButton.create(parent=widget2, text="移动到其他歌单")
-        button2 = PushButton.create(parent=widget2, text="复制到其他歌单")
-        button3 = PushButton.create(parent=widget2, text="从当前歌单移除", clicked_callback=self.removed_from_the_current_playlist)
+        button1 = PushButton.create(
+            parent=widget2, text="移动到其他歌单", clicked_callback=self.move_to_other_playlist)
+        button2 = PushButton.create(
+            parent=widget2, text="复制到其他歌单", clicked_callback=self.copy_to_other_playlist)
+        button3 = PushButton.create(
+            parent=widget2, text="从当前歌单移除", clicked_callback=self.removed_from_the_current_playlist)
 
         layout1 = Layout.create(name='QHBoxLayout', children=[button1, button2, button3])
 
@@ -94,45 +97,138 @@ class PageSongList(QScrollArea):
 
     def on_item_clicked(self, item):
         file_name = item.text()
-        self.selected_subitem = os.path.join(config_js['music_folder_path'], file_name) # 获取选中子项对应文件的绝对路径
+        self.selected_subitem_AP = os.path.join(config_js['music_folder_path'], file_name) # 获取选中子项对应文件的绝对路径
     
     def move_to_other_playlist(self) -> None:
-        pass
+        """移动到其他目录"""
+        if self.selected_subitem_AP:
+            # 检测选中的子项对应的文件是否存在
+            if os.path.exists(self.selected_subitem_AP):
+                object_file_name = os.path.basename(self.selected_subitem_AP)  # 目标文件名
+                # 文件对话框设置
+                options = QFileDialog.Options()
+                options |= QFileDialog.DontUseNativeDialog  # 使用Qt的文件对话框，而不是本地对话框
+                file_dialog = QFileDialog()
+                file_dialog.setOptions(options)
+                file_dialog.setDirectory(os.path.dirname(config_js['music_folder_path'])) # 设置文件对话框的初始位置为当前歌单的上级目录
+                # 打开文件对话框,获取用户所选目录绝对路径
+                folder_path = file_dialog.getExistingDirectory(self, '选择目标歌单')
+                # 检查目标目录是否存在同名文件
+                if os.path.exists(os.path.join(folder_path, object_file_name)):
+                    # 弹出提示框询问用户是否覆盖文件
+                    response = QMessageBox.question(
+                        self,
+                        '文件已存在',
+                        '目标目录中已存在同名文件，是否要覆盖?',
+                        QMessageBox.Yes | QMessageBox.No
+                    )
+                    if response == QMessageBox.No:
+                        return  # 如果用户选择不覆盖，则结束操作
+                # 读取目标文件内容
+                with open(self.selected_subitem_AP, 'rb') as file:
+                    content = file.read()
+                # 将目标文件添加到目标目录
+                with open(os.path.join(folder_path, object_file_name), 'wb') as file:
+                    file.write(content)
+                # 删除文件
+                os.remove(self.selected_subitem_AP)
+                # 将选中状态置空
+                self.selected_subitem_AP = None
+                # 完成提示
+                items_to_delete = self.listWidget.findItems(object_file_name, Qt.MatchExactly) # 查找包含目标文件名的项
+                # 如果找到匹配的项，删除第一个匹配项
+                if items_to_delete:
+                    item = items_to_delete[0]
+                    row = self.listWidget.row(item)
+                    self.listWidget.takeItem(row)
+            else:
+                QMessageBox.critical(self, 'FileNotFoundError', '文件不存在!', QMessageBox.Ok)
+        else:
+            QMessageBox.warning(self, 'warning', '未选中文件!', QMessageBox.Ok)
+
     def copy_to_other_playlist(self) -> None:
-        pass
+        """复制到其他目录"""
+        if self.selected_subitem_AP:
+            # 检测选中的子项对应的文件是否存在
+            if os.path.exists(self.selected_subitem_AP):
+                object_file_name = os.path.basename(self.selected_subitem_AP)  # 目标文件名
+                # 文件对话框设置
+                options = QFileDialog.Options()
+                options |= QFileDialog.DontUseNativeDialog  # 使用Qt的文件对话框，而不是本地对话框
+                file_dialog = QFileDialog()
+                file_dialog.setOptions(options)
+                file_dialog.setDirectory(os.path.dirname(config_js['music_folder_path'])) # 设置文件对话框的初始位置为当前歌单的上级目录
+                # 打开文件对话框,获取用户所选目录绝对路径
+                folder_path = file_dialog.getExistingDirectory(self, '选择目标歌单')
+                # 检查目标目录是否存在同名文件
+                if os.path.exists(os.path.join(folder_path, object_file_name)):
+                    # 弹出提示框询问用户是否覆盖文件
+                    response = QMessageBox.question(
+                        self,
+                        '文件已存在',
+                        '目标目录中已存在同名文件，是否要覆盖?',
+                        QMessageBox.Yes | QMessageBox.No
+                    )
+                    if response == QMessageBox.No:
+                        return  # 如果用户选择不覆盖，则结束操作
+                # 读取目标文件内容
+                with open(self.selected_subitem_AP, 'rb') as file:
+                    content = file.read()
+                # 将目标文件添加到目标目录
+                with open(os.path.join(folder_path, object_file_name), 'wb') as file:
+                    file.write(content)
+                # 将选中状态置空
+                self.selected_subitem_AP = None
+                # 完成提示
+                QMessageBox.information(self, 'information', "复制成功")
+            else:
+                QMessageBox.critical(self, 'FileNotFoundError', "文件不存在!", QMessageBox.Ok)
+        else:
+            QMessageBox.warning(self, 'warning', "未选中文件!", QMessageBox.Ok)
+            
     def removed_from_the_current_playlist(self) ->None:
         """从当前文件夹删除目标文件, 并添加到"最近删除"目录"""
-        if self.selected_subitem:
+        if self.selected_subitem_AP:
             song_library_path = os.path.dirname(config_js['music_folder_path']) # 获取曲库路径
             recently_deleted_directory_path = os.path.join(song_library_path, "最近删除") # "最近删除"目录路径
-            object_file_name = os.path.basename(self.selected_subitem) # 目标文件名
 
             # 检测曲库中是否有"最近删除"目录, 如果不存在，则创建"最近删除"目录
             if not os.path.exists(recently_deleted_directory_path):
                 os.makedirs(recently_deleted_directory_path)
             else:
                 # 检测选中的子项对应的文件是否存在
-                if os.path.exists(self.selected_subitem):
-                    with open(self.selected_subitem, 'rb') as file:
+                if os.path.exists(self.selected_subitem_AP):
+                    object_file_name = os.path.basename(self.selected_subitem_AP)  # 目标文件名
+                    # 检查是否已存在相同文件名的文件
+                    existing_files = [f for f in os.listdir(recently_deleted_directory_path) if f.startswith(object_file_name)]
+                    count = 1
+                    new_file_name = object_file_name
+                    # 若目标目录存在目标文件名,则使用"目标文件名(1)"依此类推
+                    while new_file_name in existing_files:
+                        count += 1
+                        base_name, extension = os.path.splitext(object_file_name)
+                        new_file_name = f"{base_name}({count}){extension}"
+                    # 读取目标文件内容
+                    with open(self.selected_subitem_AP, 'rb') as file:
                         content = file.read()
                     # 将要删除的文件添加到"最近删除"目录
-                    with open(os.path.join(recently_deleted_directory_path, object_file_name), 'wb') as file:
+                    with open(os.path.join(recently_deleted_directory_path, new_file_name), 'wb') as file:
                         file.write(content)
                     # 删除文件
-                    os.remove(self.selected_subitem)
-                    # 查找包含目标文件名的项
-                    items_to_delete = self.listWidget.findItems(object_file_name, Qt.MatchExactly)
+                    os.remove(self.selected_subitem_AP)
+                    # 将选中状态置空
+                    self.selected_subitem_AP = None
+                    # 完成提示
+                    items_to_delete = self.listWidget.findItems(object_file_name, Qt.MatchExactly) # 查找包含目标文件名的项
                     # 如果找到匹配的项，删除第一个匹配项
                     if items_to_delete:
                         item = items_to_delete[0]
                         row = self.listWidget.row(item)
                         self.listWidget.takeItem(row)
-                    # 将选中状态置空
-                    self.selected_subitem = None
                 else:
-                    QMessageBox.critical(self, 'FileNotFoundError', '文件不存在!', QMessageBox.Ok)
+                    QMessageBox.critical(self, 'FileNotFoundError', "文件不存在!", QMessageBox.Ok)
         else:
-            QMessageBox.warning(self, 'warning', '未选中文件!', QMessageBox.Ok)
+            QMessageBox.warning(self, 'warning', "未选中文件!", QMessageBox.Ok)
 
     def get_all_audio_files_in_folder(self, folder_path: str) -> list:
         """获取所选文件夹下的全部音频文件名称"""
